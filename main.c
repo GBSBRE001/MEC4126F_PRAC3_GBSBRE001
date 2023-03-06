193ea5f45e06125fb90d3eb8806f315348bb7706
GBSBRE001 <111292614+GBSBRE001@users.noreply.github.com>
2023-03-06 08:17:13 +0200

    MEC4126F_prac3_Q3.2

--- main.c
+++ main.c
@@ -19,14 +19,37 @@ struct age_data{
 	int age;
 };
 
+void main(void);			//COMPULSORY
+void init_LCD(void);
+void delay();
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 void main(void)
 {
+	struct age_data MyData;
+	MyData.date = 19;
+	MyData.month = 03;
+	MyData.year = 2001;
+	MyData.age = 21;
+
+	init_LCD();
+	char string[3];
+
+
+
 	while(1)
 	{
+		for(int i=0; i<=MyData.age; i++){
 
+			sprintf(string, "%d",i);
+			lcd_putstring(string);
+			delay(100000);
+			lcd_command(CLEAR);
+
+		}
+
+	}
 }
 
 // OTHER FUNCTIONS -----------------------------------------------------------|
